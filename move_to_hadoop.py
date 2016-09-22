6b5a4668bbf5d85d2e19cb2e96bbda5c238e1c7c
Juliet Hougland <not@myemail.com>
2016-09-22 15:33:25 -0700

    Add args for move_to_hadoop.py

--- move_to_hadoop.py
+++ move_to_hadoop.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import ibis
 import os
 import pandas as pd
@@ -5,11 +6,6 @@ import pandas as pd
 
 ibis.options.sql.default_limit = None
 
-hdfs_conn = ibis.hdfs_connect(host='bottou03.sjc.cloudera.com')
-
-ibis_conn = ibis.impala.connect(host='bottou01.sjc.cloudera.com',
-                                port=21050,
-                                hdfs_client=hdfs_conn)
 
 FILE_SCHEMA = ibis.schema([('project_name', 'string'),
                            ('page_name', 'string'),
@@ -19,12 +15,9 @@ FILE_SCHEMA = ibis.schema([('project_name', 'string'),
 # System independent way to join paths
 LOCAL_DATA_PATH = os.path.join(os.getcwd(), "pageviews-gz")
 LOCAL_FILES = os.listdir(LOCAL_DATA_PATH)
-HDFS_DIR='/user/juliet/pageviews-gz'
-DB_NAME='u_juliet'
 
-
-def mv_files(filename):
-    dir_name = HDFS_DIR + filename[:-3]
+def mv_files(filename, hdfs_dir, hdfs_conn):
+    dir_name = hdfs_dir + filename[:-3]
     hdfs_conn.mkdir(dir_name)
     filepathtarget = '/'.join([dir_name, filename])
     hdfs_conn.put(filepathtarget, os.path.join(LOCAL_DATA_PATH, filename))
@@ -44,7 +37,7 @@ def to_pd_dt(filename):
     return pd.to_datetime(filename, format='pageviews-%Y%m%d-%H0000')
 
 
-def gz_2_data_insert(data_dir):
+def gz_2_data_insert(data_dir, ibis_conn, db_name):
     tmp_table = ibis_conn.delimited_file(hdfs_dir=data_dir,
                                   schema=FILE_SCHEMA,
                                   delimiter=' ')
@@ -52,14 +45,14 @@ def gz_2_data_insert(data_dir):
     # create a column named time
     tmp_w_time = tmp_table.mutate(year=year, month=month, day=day, hour=hour)
 
-    working_db = safe_get_db(DB_NAME)
+    working_db = safe_get_db(ibis_conn, db_name)
     if 'wiki_pageviews' in working_db.tables:
         ibis_conn.insert('wiki_pageviews', tmp_w_time, database=DB_NAME)
     else:
         ibis_conn.create_table('wiki_pageviews', obj=tmp_w_time,
-                               database=DB_NAME)
+                               database=db_name)
 
-def safe_get_db(db_name):
+def safe_get_db(ibis_conn, db_name):
     if ibis_conn.exists_database(db_name):
         user_db = ibis_conn.database(db_name)
     else:
@@ -67,10 +60,28 @@ def safe_get_db(db_name):
     return user_db
 
 
-def main():
-    hdfs_gz_dirs = [mv_files(filename) for filename in LOCAL_FILES]
-    [gz_2_data_insert(data_dir) for data_dir in hdfs_gz_dirs]
+def main(hdfs_conn, ibis_conn, hdfs_dir, db_name):
+    hdfs_gz_dirs = [mv_files(filename, hdfs_dir, hdfs_conn) for filename in
+                    LOCAL_FILES]
+    [gz_2_data_insert(data_dir, ibis_conn, db_name) for data_dir in hdfs_gz_dirs]
 
 
 if __name__ == "__main__":
-    main()
+    arg_parser = ArgumentParser()
+    # Default to January 1st, 2016
+    arg_parser.add_argument("--db_name",
+                            default='u_juliet')
+    arg_parser.add_argument("--hdfs_dir",
+                            default='/user/juliet/pageviews-gz')
+    arg_parser.add_argument("--nn_host",
+                            default='bottou03.sjc.cloudera.com')
+    arg_parser.add_argument("--impala_host",
+                            default='bottou01.sjc.cloudera.com')
+    args = arg_parser.parse_args()
+
+    hdfs_conn = ibis.hdfs_connect(host=args.nn_host)
+    ibis_conn = ibis.impala.connect(host=args.impala_host,
+                                port=21050,
+                                hdfs_client=hdfs_conn)
+
+    main(hdfs_conn, ibis_conn, args.hdfs_dir, args.db_name)
