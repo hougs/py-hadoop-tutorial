48973001be6c7c4fa5bdfe9dc903ca004c294b39
Juliet Hougland <not@myemail.com>
2016-09-22 13:24:47 -0700

    Add agg parsing and facotr out into a few simpler methods.

--- move_to_hadoop.py
+++ move_to_hadoop.py
@@ -19,10 +19,12 @@ FILE_SCHEMA = ibis.schema([('project_name', 'string'),
 # System independent way to join paths
 LOCAL_DATA_PATH = os.path.join(os.getcwd(), "pageviews-gz")
 LOCAL_FILES = os.listdir(LOCAL_DATA_PATH)
+HDFS_DIR='/user/juliet/pageviews-gz'
+DB_NAME='u_juliet'
 
 
 def mv_files(filename):
-    dir_name = '/user/juliet/pageviews-gz/{}'.format(filename[:-3])
+    dir_name = HDFS_DIR + filename[:-3]
     hdfs_conn.mkdir(dir_name)
     filepathtarget = '/'.join([dir_name, filename])
     hdfs_conn.put(filepathtarget, os.path.join(LOCAL_DATA_PATH, filename))
@@ -42,30 +44,32 @@ def to_pd_dt(filename):
     return pd.to_datetime(filename, format='pageviews-%Y%m%d-%H0000')
 
 
-def gz_2_data_insert(data_dir, db_expr):
+def gz_2_data_insert(data_dir):
     tmp_table = ibis_conn.delimited_file(hdfs_dir=data_dir,
                                   schema=FILE_SCHEMA,
                                   delimiter=' ')
     year, month, day, hour = extract_datetime(data_dir.split("/")[-1])
     # create a column named time
     tmp_w_time = tmp_table.mutate(year=year, month=month, day=day, hour=hour)
-    if 'wiki_pageviews' in db_expr.tables:
-        ibis_conn.insert('wiki_pageviews', tmp_w_time, database='u_juliet')
+
+    working_db = safe_get_db(DB_NAME)
+    if 'wiki_pageviews' in working_db.tables:
+        ibis_conn.insert('wiki_pageviews', tmp_w_time, database=DB_NAME)
     else:
-        ibis_conn.create_table('wiki_pageviews', obj=tmp_w_time, database='u_juliet')
+        ibis_conn.create_table('wiki_pageviews', obj=tmp_w_time,
+                               database=DB_NAME)
+
+def safe_get_db(db_name):
+    if ibis_conn.exists_database(db_name):
+        user_db = ibis_conn.database(db_name)
+    else:
+        user_db = ibis_conn.create_database(db_name)
+    return user_db
 
 
 def main():
     hdfs_gz_dirs = [mv_files(filename) for filename in LOCAL_FILES]
-
-    db_name = 'u_juliet'
-    if ibis_conn.exists_database(db_name):
-        user_db = ibis_conn.database(db_name)
-    else:
-        user_db = ibis_conn.create_database('u_juliet')
-
-    [gz_2_data_insert(data_dir, user_db) for data_dir in hdfs_gz_dirs]
-
+    [gz_2_data_insert(data_dir) for data_dir in hdfs_gz_dirs]
 
 
 if __name__ == "__main__":
