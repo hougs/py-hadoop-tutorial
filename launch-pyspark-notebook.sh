955a5977f1f53cfdcb04dae3ef450280e2591474
Juliet Hougland <not@myemail.com>
2016-09-25 14:07:30 -0400

    Change limits for driver and executor memory.

--- launch-pyspark-notebook.sh
+++ launch-pyspark-notebook.sh
@@ -6,10 +6,10 @@ export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.ip='*' --NotebookApp.o
 export HADOOP_CONF_DIR=/etc/hive/conf
 export HIVE_CP=/opt/cloudera/parcels/CDH/lib/hive/lib/
 
-pyspark --master local[2] --deploy-mode client --driver-memory 4G \
+pyspark --master local[2] --deploy-mode client --driver-memory 2G \
     --driver-class-path $HIVE_CP \
     --conf spark.executor.extraClassPath=$HIVE_CP \
-    --conf spark.yarn.executor.memoryOverhead=1024
+    --conf spark.yarn.executor.memoryOverhead=2048
 
 
 
