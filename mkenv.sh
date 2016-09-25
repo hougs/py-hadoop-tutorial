1a51837e94d1a53bf3ddb6a86c2e8110cde9fd73
Juliet Hougland <not@myemail.com>
2016-09-25 15:51:14 -0400

    update commented out parts.

--- mkenv.sh
+++ mkenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#sudo apt-get install git
-#git clone https://github.com/jhlch/py-hadoop-tutorial.git
+sudo apt-get install git
+git clone https://github.com/jhlch/py-hadoop-tutorial.git
 
-#mkdir miniconda
-#cd miniconda
-#curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-#chmod +x Miniconda3-latest-Linux-x86_64.sh
-#./Miniconda3-latest-Linux-x86_64.sh
+mkdir miniconda
+cd miniconda
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
 export PATH=/home/srowen/miniconda3/bin:$PATH
 conda create --name pyhadoop pandas
 source activate pyhadoop
