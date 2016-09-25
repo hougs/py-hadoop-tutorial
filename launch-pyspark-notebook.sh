fa690f1ca734498283dc15ac68338a458460a696
Juliet Hougland <not@myemail.com>
2016-09-25 15:23:10 -0400

    Change resources for notebooks.

--- launch-pyspark-notebook.sh
+++ launch-pyspark-notebook.sh
@@ -6,10 +6,6 @@ export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.ip='*' --NotebookApp.o
 export HADOOP_CONF_DIR=/etc/hive/conf
 export HIVE_CP=/opt/cloudera/parcels/CDH/lib/hive/lib/
 
-pyspark --master local[2] --deploy-mode client --driver-memory 2G \
-    --driver-class-path $HIVE_CP \
-    --conf spark.executor.extraClassPath=$HIVE_CP \
-    --conf spark.yarn.executor.memoryOverhead=2048
-
-
+pyspark --master yarn --deploy-mode client --driver-memory 2g \
+--num-executors 1 --executor-memory 8g --executor-cores 3
 
