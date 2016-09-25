e1c5e9cd0f00a6e0245587530207d2bdbeeef2fb
Juliet Hougland <not@myemail.com>
2016-09-25 12:28:19 -0400

    Add notebook launching and add importing of pyspark.

---
+++ launch-pyspark-notebook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PYSPARK_DRIVER_PYTHON=ipython
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.ip='*' --NotebookApp.open_browser=False --NotebookApp.port=8880"
+
+export HADOOP_CONF_DIR=/etc/hive/conf
+export HIVE_CP=/opt/cloudera/parcels/CDH/lib/hive/lib/
+
+pyspark --master local[2] --deploy-mode client --driver-memory 4G \
+    --driver-class-path $HIVE_CP \
+    --conf spark.executor.extraClassPath=$HIVE_CP \
+    --conf spark.yarn.executor.memoryOverhead=1024
+
+
+
