85353bd7d529fc3360a59dc30004c0145834f282
Juliet Hougland <not@myemail.com>
2016-09-25 15:45:05 -0400

    Adds env creation for nodes running on jupyter on cluster.

---
+++ mkenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sudo apt-get install git
+#git clone https://github.com/jhlch/py-hadoop-tutorial.git
+
+#mkdir miniconda
+#cd miniconda
+#curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#chmod +x Miniconda3-latest-Linux-x86_64.sh
+#./Miniconda3-latest-Linux-x86_64.sh
+export PATH=/home/srowen/miniconda3/bin:$PATH
+conda create --name pyhadoop pandas
+source activate pyhadoop
+pip install -r py-hadoop-tutorial/requirements.txt
+
